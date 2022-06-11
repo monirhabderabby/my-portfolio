a447941585f9317551b7f122aa7fe4ecd57b03e0
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-06-11 21:26:13 +0600

    project card responsive issue fix

--- src/Components/Shared/ProjectCard.js
+++ src/Components/Shared/ProjectCard.js
@@ -5,7 +5,7 @@ import './ProjectCard.css'
 const ProjectCard = ({ data }) => {
     return (
         <div>
-            <div class="card mx-auto sm:w-24 lg:w-96 bg-base-100 shadow-xl">
+            <div class="card mx-auto sm:w-24 md:w-72 lg:w-96 bg-base-100 shadow-xl">
                 <figure>
                     <img
                         src={data?.img}
@@ -16,7 +16,7 @@ const ProjectCard = ({ data }) => {
                     <h2 class="card-title">
                         {data?.name}
                     </h2>
-                    <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
+                    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
                         <div class="badge badge-secondary">React</div>
                         <div class="badge badge-secondary">NodeJs</div>
                         <div class="badge badge-secondary">NodeJs</div>
